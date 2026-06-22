de569da300c1dfce40722b8105b40583b32cd437
HarithaBuddiga <buddigaharitha15@gmail.com>
2026-06-23 00:12:03 +0530

    Fix: Dynamically bind to Render environment PORT

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Start Server
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
+
